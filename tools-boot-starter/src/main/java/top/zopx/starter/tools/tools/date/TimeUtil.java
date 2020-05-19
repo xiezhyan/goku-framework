6ba9e9a837faeee34b086107db5f664c821840b8
bin_cruel@sohu.com <bin_cruel@sohu.com>
2020-05-19 12:03:03 +0800

    Update TimeUtil.java

--- tools-boot-starter/src/main/java/top/zopx/starter/tools/tools/date/TimeUtil.java
+++ tools-boot-starter/src/main/java/top/zopx/starter/tools/tools/date/TimeUtil.java
@@ -13,11 +13,11 @@ import java.time.LocalTime;
  */
 public class TimeUtil {
 
-    public static int getTime(double time) {
-        return StringUtil.toInteger((time + RandomUtils.nextInt(0, 180)));
+    public static long getTime(double time) {
+        return StringUtil.toInteger((time + RandomUtils.nextLong(0, 180)));
     }
 
-    public static int to24() {
-        return LocalDateUtils.to24() + RandomUtils.nextInt(0, 180);
+    public static long to24() {
+        return getTime(LocalDateUtils.to24());
     }
 }
