3e06aa35f7a714f3d65e5b730f70292f0da5611c
bin_cruel@sohu.com <bin_cruel@sohu.com>
2020-11-20 07:34:40 +0800

    validator

---
+++ tools-boot-starter/src/main/java/top/zopx/starter/tools/validate/annotations/MutiPattern.java
@@ -0,0 +1,35 @@
+package top.zopx.starter.tools.validate.annotations;
+
+import top.zopx.starter.tools.validate.listeners.MutiPatternValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author sanq.Yan
+ * @date 2020/11/19
+ */
+@Target(value = ElementType.FIELD)
+@Retention(value = RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = MutiPatternValidator.class)
+public @interface MutiPattern {
+
+    String message() default "";
+
+    String[] patterns() default {};
+
+    Logic logic() default Logic.AND;
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    enum Logic {
+        AND,
+        OR
+    }
+}
---
+++ tools-boot-starter/src/main/java/top/zopx/starter/tools/validate/listeners/MutiPatternValidator.java
@@ -0,0 +1,49 @@
+package top.zopx.starter.tools.validate.listeners;
+
+import org.apache.commons.collections4.CollectionUtils;
+import top.zopx.starter.tools.tools.strings.MatcherUtil;
+import top.zopx.starter.tools.validate.annotations.MutiPattern;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author sanq.Yan
+ * @date 2020/11/19
+ */
+public class MutiPatternValidator implements ConstraintValidator<MutiPattern, String> {
+
+    private MutiPattern.Logic logic;
+    private List<String> patterns;
+
+    @Override
+    public void initialize(MutiPattern constraintAnnotation) {
+        logic = constraintAnnotation.logic();
+        patterns = Arrays.asList(constraintAnnotation.patterns());
+    }
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        if (CollectionUtils.isEmpty(patterns))
+            return false;
+
+        if (logic == MutiPattern.Logic.OR) {
+            for (String pattern : patterns) {
+                if (MatcherUtil.match(pattern, s)) {
+                    return true;
+                }
+            }
+        } else if (logic == MutiPattern.Logic.AND) {
+            for (String pattern : patterns) {
+                if (!MatcherUtil.match(pattern, s)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        return false;
+    }
+}
