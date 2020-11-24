e1c096408be21f806338e6583bfda2550a3a2083
Mr.sanq <xiezhyan@126.com>
2020-11-24 17:02:46 +0800

    Create Entity.java

---
+++ tools-boot-starter/src/main/java/top/zopx/starter/tools/basic/Entity.java
@@ -0,0 +1,27 @@
+package top.zopx.starter.tools.basic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 基础实体类
+ *  - 典型案例：数据权限参数配置
+ *
+ * @author sanq.Yan
+ * @date 2020/11/24
+ */
+public class Entity {
+
+    /**
+     * 自定义参数
+     */
+    private Map<String, Object> map = new HashMap<>();
+
+    public Map<String, Object> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, Object> map) {
+        this.map = map;
+    }
+}
