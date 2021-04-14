b061f9daaadcc912682f55dd768bb45302eb8678
xiezhongyan@123.com <xiezhyan@126.com>
2021-04-14 21:12:17 +0800

    .

--- square-log-spring-boot-starter/src/main/java/top/zopx/starter/log/properties/SquareLogProperties.java
+++ square-log-spring-boot-starter/src/main/java/top/zopx/starter/log/properties/SquareLogProperties.java
@@ -2,7 +2,6 @@ package top.zopx.starter.log.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import top.zopx.starter.tools.configurator.basic.SquareProperties;
 
 import java.lang.ref.PhantomReference;
 
