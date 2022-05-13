9b25a21b0b6f51233e3b92f79b5a0ac7d48757ca
俗世游子 <xiezhyan@126.com>
2022-05-13 18:05:29 +0800

    Update PublishEventListener.java

--- square-log-spring-boot-starter/src/main/java/top/zopx/starter/log/event/listener/PublishEventListener.java
+++ square-log-spring-boot-starter/src/main/java/top/zopx/starter/log/event/listener/PublishEventListener.java
@@ -1,15 +1,17 @@
 package top.zopx.starter.log.event.listener;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import top.zopx.starter.log.configurator.properties.SquareLogProperties;
 import top.zopx.starter.log.constant.LogConstant;
 import top.zopx.starter.log.event.ApiLogEvent;
 import top.zopx.starter.log.event.ErrorLogEvent;
-import top.zopx.starter.log.configurator.properties.SquareLogProperties;
 import top.zopx.starter.log.service.ILogService;
 
 import javax.annotation.Resource;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * @author 俗世游子
@@ -24,11 +26,14 @@ public class PublishEventListener {
     @Resource
     private SquareLogProperties squareLogProperties;
 
+    @Value("${spring.application.name}")
+    private String applicationName;
+
     @EventListener(value = ErrorLogEvent.class)
     @SuppressWarnings("all")
     public void onApplicationEvent(ErrorLogEvent event) {
         final Map<String, Object> source = (Map<String, Object>) event.getSource();
-        source.put(LogConstant.APP_NAME, squareLogProperties.getAppName());
+        source.put(LogConstant.APP_NAME, Optional.ofNullable(squareLogProperties.getAppName()).orElse(applicationName));
         logService.saveError(source);
     }
 
@@ -36,7 +41,7 @@ public class PublishEventListener {
     @SuppressWarnings("all")
     public void onApplicationEvent(ApiLogEvent event) {
         final Map<String, Object> source = (Map<String, Object>) event.getSource();
-        source.put(LogConstant.APP_NAME, squareLogProperties.getAppName());
+        source.put(LogConstant.APP_NAME, Optional.ofNullable(squareLogProperties.getAppName()).orElse(applicationName));
         logService.saveApi(source);
     }
 }
