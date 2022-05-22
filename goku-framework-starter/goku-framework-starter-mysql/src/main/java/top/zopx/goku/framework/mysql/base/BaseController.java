f932fed0721d3f135e72da8014cb7c1b22224baf
俗世游子 <xiezhyan@126.com>
2022-05-22 10:28:36 +0800

    mysql base

---
+++ goku-framework-starter/goku-framework-starter-mysql/src/main/java/top/zopx/goku/framework/mysql/base/BaseController.java
@@ -0,0 +1,100 @@
+package top.zopx.goku.framework.mysql.base;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import top.zopx.goku.framework.log.annotations.OperatorLogAnnotation;
+import top.zopx.goku.framework.mysql.entity.DataEntity;
+import top.zopx.goku.framework.tools.entity.vo.BaseEntity;
+import top.zopx.goku.framework.tools.entity.vo.Page;
+import top.zopx.goku.framework.tools.entity.vo.Pagination;
+import top.zopx.goku.framework.tools.entity.wrapper.R;
+
+import javax.validation.Valid;
+import java.util.function.LongConsumer;
+import java.util.stream.Collectors;
+
+/**
+ * 基础Controller
+ *
+ * @author 俗世游子
+ * @date 2022/05/12
+ */
+public abstract class BaseController<
+        VO extends BaseEntity,
+        DTO,
+        Entity extends DataEntity,
+        Service extends IBaseService<DTO, Entity>> {
+
+    /**
+     * 获取服务
+     *
+     * @return IBaseService
+     */
+    @Autowired
+    protected Service baseService;
+
+    @GetMapping
+    @OperatorLogAnnotation(value = "获取数据列表")
+    public R<Page<VO>> getList(
+            Pagination pagination,
+            VO vo
+    ) {
+        LongConsumer consumer = null;
+        if (null != pagination) {
+            consumer = pagination::setTotalCount;
+        }
+        return R.result(
+                new Page<>(
+                        pagination,
+                        baseService.getList(pagination, convertToDTO(vo), consumer)
+                                .stream()
+                                .map(this::convertToVO)
+                                .collect(Collectors.toList())
+                )
+        );
+    }
+
+    @GetMapping("/{id}")
+    @OperatorLogAnnotation(value = "获取数据详情")
+    public R<VO> getByPriKey(
+            @PathVariable("id") Long id
+    ) {
+        return R.result(
+                convertToVO(baseService.getByPriKey(id))
+        );
+    }
+
+    @PostMapping
+    @OperatorLogAnnotation(value = "保存")
+    public R<Boolean> save(@Valid @RequestBody VO vo) {
+        return R.result(baseService.create(convertToDTO(vo)));
+    }
+
+    @PutMapping("/{id}")
+    @OperatorLogAnnotation(value = "通过主键修改")
+    public R<Boolean> updateByPriKey(@Valid @RequestBody VO vo, @PathVariable("id") Long id) {
+        return R.result(baseService.updateByPriKey(convertToDTO(vo), id));
+    }
+
+    @DeleteMapping("/{id}")
+    @OperatorLogAnnotation(value = "通过主键删除")
+    public R<Boolean> deleteByPriKey(@PathVariable("id") Long id) {
+        return R.result(baseService.deleteByPriKey(id));
+    }
+
+    /**
+     * 将VO转换为DTO
+     *
+     * @param vo VO对象
+     * @return DTO
+     */
+    protected abstract DTO convertToDTO(VO vo);
+
+    /**
+     * 将DTO转换为VO
+     *
+     * @param dto DTO对象
+     * @return VO
+     */
+    protected abstract VO convertToVO(DTO dto);
+}
---
+++ goku-framework-starter/goku-framework-starter-mysql/src/main/java/top/zopx/goku/framework/mysql/base/IHookService.java
@@ -0,0 +1,68 @@
+package top.zopx.goku.framework.mysql.base;
+
+
+/**
+ * 钩子函数
+ *
+ * @author 俗世游子
+ * @date 2022/05/12
+ */
+public interface IHookService<DTO, Entity> {
+
+    /**
+     * 查询钩子函数
+     *
+     * @param dto 入参
+     */
+    default void doSearchBefore(DTO dto) {
+    }
+
+    /**
+     * 修改钩子函数
+     *
+     * @param data 入参
+     * @param dto  入参
+     */
+    default void doUpdateBefore(Entity data, DTO dto) {
+    }
+
+    /**
+     * 修改钩子函数
+     *
+     * @param data 入参
+     * @return 是否处理成功
+     */
+    default Boolean doUpdateAfter(Entity data) {
+        return true;
+    }
+
+    /**
+     * 创建钩子函数
+     *
+     * @param dto    入参
+     * @param entity 入参
+     */
+    default void doCreateBefore(Entity entity, DTO dto) {
+    }
+
+    /**
+     * 创建成功之后的
+     *
+     * @param entity 参数
+     * @return 是否处理成功
+     */
+    default Boolean doCreateAfter(Entity entity, DTO dto) {
+        return true;
+    }
+
+    /**
+     * 删除钩子函数
+     *
+     * @param id 主键
+     * @return Entity
+     */
+    default Entity doDeleteBefore(Long id) {
+        return null;
+    }
+
+}
