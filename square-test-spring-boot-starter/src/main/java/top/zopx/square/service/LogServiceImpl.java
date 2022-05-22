edf8e40543a8480a1b821d76a28d2bbd287253fc
俗世游子 <xiezhyan@126.com>
2022-05-22 10:14:28 +0800

    test

---
+++ square-test-spring-boot-starter/src/main/java/top/zopx/square/service/LogServiceImpl.java
@@ -0,0 +1,22 @@
+package top.zopx.square.service;
+
+import org.springframework.stereotype.Component;
+import top.zopx.goku.framework.log.service.ILogService;
+
+import java.util.Map;
+
+/**
+ * @author 俗世游子
+ * @email xiezhyan@126.com
+ * @date 2022/05/22 9:59
+ */
+@Component
+public class LogServiceImpl implements ILogService {
+    public void saveError(Map<String, Object> map) {
+
+    }
+
+    public void saveApi(Map<String, Object> map) {
+
+    }
+}
--- square-test-spring-boot-starter/src/test/java/top/zopx/square/test/a.java
+++
@@ -1,9 +0,0 @@
-package top.zopx.square.test;
-
-/**
- * @author 【{}】
- * @email 【{}】
- * @date 2021/7/1
- */
-public class a {
-}
