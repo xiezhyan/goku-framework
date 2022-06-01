f625fe3e8f21606fa3ef8191a4231706c268d2e2
俗世游子 <xiezhyan@126.com>
2022-06-01 17:44:18 +0800

    0

--- square-test-spring-boot-starter/src/main/java/top/zopx/square/TestApplication.java
+++ square-test-spring-boot-starter/src/main/java/top/zopx/square/TestApplication.java
@@ -3,6 +3,7 @@ package top.zopx.square;
 import io.minio.MinioClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.Cacheable;
 
 import javax.annotation.Resource;
 
@@ -20,4 +21,9 @@ public class TestApplication {
         SpringApplication.run(TestApplication.class, args);
     }
 
+    @Cacheable
+
+    public void test() {
+
+    }
 }
