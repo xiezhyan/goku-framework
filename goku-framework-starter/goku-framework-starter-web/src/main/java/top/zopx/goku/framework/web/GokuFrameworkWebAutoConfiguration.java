d04b77cb3f81f556549f8870333f39673ff26d49
俗世游子 <xiezhyan@126.com>
2022-06-23 18:04:22 +0800

    Update GokuFrameworkWebAutoConfiguration.java

--- goku-framework-starter/goku-framework-starter-web/src/main/java/top/zopx/goku/framework/web/GokuFrameworkWebAutoConfiguration.java
+++ goku-framework-starter/goku-framework-starter-web/src/main/java/top/zopx/goku/framework/web/GokuFrameworkWebAutoConfiguration.java
@@ -3,11 +3,9 @@ package top.zopx.goku.framework.web;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import top.zopx.goku.framework.web.util.LogHelper;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
 
 /**
  * @author 俗世游子
@@ -19,27 +17,22 @@ import javax.annotation.Resource;
 @ComponentScan("top.zopx.goku.framework.web")
 public class GokuFrameworkWebAutoConfiguration {
 
-    @Resource
-    private ThreadPoolTaskExecutor taskExecutor;
-
     @PostConstruct
     public void banner() {
-        taskExecutor.execute(() ->
-            LogHelper.getLogger(getClass())
-                    .info(
-                            "\n{}",
-                            "      _,---.     _,.---._    ,--.-.,-.\n" +
-                                    "  _.='.'-,  \\  ,-.' , -  `. /==/- |\\  \\ .--.-. .-.-.\n" +
-                                    " /==.'-     / /==/_,  ,  - \\|==|_ `/_ //==/ -|/=/  |\n" +
-                                    "/==/ -   .-' |==|   .=.     |==| ,   / |==| ,||=| -|\n" +
-                                    "|==|_   /_,-.|==|_ : ;=:  - |==|-  .|  |==|- | =/  |\n" +
-                                    "|==|  , \\_.' )==| , '='     |==| _ , \\ |==|,  \\/ - |\n" +
-                                    "\\==\\-  ,    ( \\==\\ -    ,_ //==/  '\\  ||==|-   ,   /\n" +
-                                    " /==/ _  ,  /  '.='. -   .' \\==\\ /\\=\\.'/==/ , _  .'\n" +
-                                    " `--`------'     `--`--''    `--`      `--`..---'\n" +
-                                    " GoKu:       1.5.1.220701"
-                    )
-        );
+        LogHelper.getLogger(getClass())
+                .info(
+                        "\n{}",
+                        "      _,---.     _,.---._    ,--.-.,-.\n" +
+                                "  _.='.'-,  \\  ,-.' , -  `. /==/- |\\  \\ .--.-. .-.-.\n" +
+                                " /==.'-     / /==/_,  ,  - \\|==|_ `/_ //==/ -|/=/  |\n" +
+                                "/==/ -   .-' |==|   .=.     |==| ,   / |==| ,||=| -|\n" +
+                                "|==|_   /_,-.|==|_ : ;=:  - |==|-  .|  |==|- | =/  |\n" +
+                                "|==|  , \\_.' )==| , '='     |==| _ , \\ |==|,  \\/ - |\n" +
+                                "\\==\\-  ,    ( \\==\\ -    ,_ //==/  '\\  ||==|-   ,   /\n" +
+                                " /==/ _  ,  /  '.='. -   .' \\==\\ /\\=\\.'/==/ , _  .'\n" +
+                                " `--`------'     `--`--''    `--`      `--`..---'\n" +
+                                " GoKu:       1.5.1.220701"
+                );
     }
 
 }
