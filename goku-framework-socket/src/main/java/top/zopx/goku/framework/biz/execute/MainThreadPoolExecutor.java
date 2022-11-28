1919ba4120385b08cf3505dea47f00b04f225232
俗世游子 <xiezhyan@126.com>
2022-11-28 15:34:55 +0800

    Update MainThreadPoolExecutor.java

--- goku-framework-socket/src/main/java/top/zopx/goku/framework/biz/execute/MainThreadPoolExecutor.java
+++ goku-framework-socket/src/main/java/top/zopx/goku/framework/biz/execute/MainThreadPoolExecutor.java
@@ -14,7 +14,39 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * 异步执行
+ * <pre>
+ * {@code
+ *  public final class MainThreadPoolExecutorSingleton {
  *
+ *     private static final Logger LOGGER = LoggerFactory.getLogger(MainThreadPoolExecutorSingleton.class);
+ *
+ *     private static final MainThreadPoolExecutor EXECUTOR = new MainThreadPoolExecutor("goku-example", new BaseCmdHandlerFactory() {
+ *         @Override
+ *         public String getScanPackage() {
+ *             return "top.zopx.goku.example.socket.biz.cmd";
+ *         }
+ *     });
+ *
+ *     private MainThreadPoolExecutorSingleton() {}
+ *
+ *     public static MainThreadPoolExecutorSingleton getInstance() {
+ *         return Holder.INSTANCE;
+ *     }
+ *
+ *     private static class Holder {
+ *         public static final MainThreadPoolExecutorSingleton INSTANCE = new MainThreadPoolExecutorSingleton();
+ *     }
+ *
+ *     public void process(Runnable runnable) {
+ *         EXECUTOR.process(runnable);
+ *     }
+ *
+ *     public void process(BaseCmdHandleContext ctx, GeneratedMessageV3 cmdMsg) {
+ *         EXECUTOR.process(ctx, cmdMsg);
+ *     }
+ * }
+ * }
+ * </pre>
  * @author 俗世游子
  * @date 2022/2/4
  * @email xiezhyan@126.com
