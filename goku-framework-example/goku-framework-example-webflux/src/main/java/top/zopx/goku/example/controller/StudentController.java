6ff76739ae94773ab62bfe03eacfd940d565dcd9
俗世游子 <xiezhyan@126.com>
2022-12-06 15:05:49 +0800

    .

--- goku-framework-example/goku-framework-example-webflux/src/main/java/top/zopx/goku/example/controller/StudentController.java
+++ goku-framework-example/goku-framework-example-webflux/src/main/java/top/zopx/goku/example/controller/StudentController.java
@@ -1,5 +1,9 @@
 package top.zopx.goku.example.controller;
 
+import org.apache.commons.lang3.RandomUtils;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -10,6 +14,7 @@ import top.zopx.goku.example.func_web.entity.Student;
 import top.zopx.goku.example.service.IStudentService;
 
 import javax.annotation.Resource;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -42,5 +47,13 @@ public class StudentController {
         return studentService.save(student);
     }
 
-
+    @GetMapping(value = "sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> sse() {
+        Flux<String> flux =
+                Flux.fromArray(new String[]{"javaboy", "itboyhub", "www.javaboy.org", "itboyhub.com"})
+                        .map(s -> {
+                            return "my->data->" + s;
+                        }).delayElements(Duration.ofMillis(500));
+        return flux;
+    }
 }
--- goku-framework-example/goku-framework-example-webflux/src/main/java/top/zopx/goku/example/controller/UploadFileController.java
+++ goku-framework-example/goku-framework-example-webflux/src/main/java/top/zopx/goku/example/controller/UploadFileController.java
@@ -1,6 +1,7 @@
 package top.zopx.goku.example.controller;
 
 import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,10 @@ import reactor.core.scheduler.Schedulers;
 import java.io.File;
 import java.io.IOException;
 import java.io.SequenceInputStream;
+import java.nio.channels.AsynchronousFileChannel;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 /**
  * @author 谢先生
@@ -42,4 +46,28 @@ public class UploadFileController {
                 });
     }
 
+    @PostMapping("/uploadSingle")
+    public Mono<String> single(@RequestPart ("file") Mono<FilePart> file) throws IOException {
+        return file.map(filePart->{
+            Path tempFile = null;
+            try {
+                tempFile = Files.createTempFile("single", filePart.filename());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            AsynchronousFileChannel channel = null;
+            try {
+                channel = AsynchronousFileChannel.open(tempFile, StandardOpenOption.WRITE);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            DataBufferUtils.write(filePart.content(), channel, 0).doOnComplete(() -> {
+                System.out.println("finish....");
+            }).subscribe();
+            return tempFile;
+        }).map(a->{
+            return a.toFile();
+            //此时已转换为File类，具体的业务逻辑我就忽略了
+        }).flatMap(fileSinge-> Mono.justOrEmpty(fileSinge.getAbsolutePath()));
+    }
 }
